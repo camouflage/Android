b0b80b288177c9833d018e46dc6a59c0e4c393e6
camouflage <363419254@qq.com>
2015-12-06 13:11:56 +0800

    Finish lab7 extra.

--- lab7/src/main/java/com/example/sunsheng/lab7/MainActivity.java
+++ lab7/src/main/java/com/example/sunsheng/lab7/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.sunsheng.lab7;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -21,6 +29,9 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     MyDatabaseHelper myDBHelper = new MyDatabaseHelper(this);
+    ListView lv;
+    List<Map<String, Object>> dataList = new ArrayList<Map<String, Object>>();
+    SimpleAdapter sa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button add = (Button) findViewById(R.id.add);
-        ListView lv = (ListView) findViewById(R.id.lv);
+        lv = (ListView) findViewById(R.id.lv);
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,9 +51,85 @@ public class MainActivity extends AppCompatActivity {
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                LayoutInflater inflater = getLayoutInflater();
+                final View layout = inflater.inflate(R.layout.modify, (ViewGroup) findViewById(R.id.modify));
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("请输入要修改的信息");
+                builder.setView(layout);
 
-                
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+
+                        // http://blog.csdn.net/mal327/article/details/7011052
+                        String modifyStuId = ((EditText) layout.findViewById(R.id.modifyStuId)).getText().toString();
+                        String modifyName = ((EditText) layout.findViewById(R.id.modifyName)).getText().toString();
+                        String modifyTel = ((EditText) layout.findViewById(R.id.modifyTel)).getText().toString();
+
+                        if (!modifyStuId.isEmpty() && !modifyName.isEmpty()) {
+                            myDBHelper.update(new Contact(modifyStuId, modifyName, modifyTel));
+
+                            // http://stackoverflow.com/questions/1669885/what-happens-when-a-duplicate-key-is-put-into-a-hashmap
+                            Map<String, Object> data = dataList.get(position);
+                            data.put("stuId", modifyStuId);
+                            data.put("name", modifyName);
+                            data.put("tel", modifyTel);
+
+                            sa.notifyDataSetChanged();
+                        } else {
+                            Toast toast = Toast.makeText(MainActivity.this, "Text cannot be empty", Toast.LENGTH_LONG);
+                            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                            toast.show();
+                        }
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.create().show();
+            }
+        });
+
+        // http://www.cnblogs.com/salam/archive/2010/11/15/1877512.html
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, final View view, final int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Do you want to delete?");
+                builder.setTitle("Attention");
+
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        myDBHelper.delete(new Contact(
+                                ((TextView) view.findViewById(R.id.stuId)).getText().toString(),
+                                ((TextView) view.findViewById(R.id.name)).getText().toString(),
+                                ((TextView) view.findViewById(R.id.tel)).getText().toString()
+                        ));
+
+                        dataList.remove(position);
+                        sa.notifyDataSetChanged();
+                    }
+                });
+
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.create().show();
+
+                return true;
             }
         });
     }
@@ -63,11 +150,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-        ListView lv = (ListView) findViewById(R.id.lv);
-        List<Map<String, Object>> dataList = new ArrayList<Map<String, Object>>();
+        lv = (ListView) findViewById(R.id.lv);
 
         setData(dataList);
-        SimpleAdapter sa = new SimpleAdapter(this, dataList, R.layout.contact,
+        sa = new SimpleAdapter(this, dataList, R.layout.contact,
                 new String[] { "stuId", "name", "tel" }, new int[] { R.id.stuId, R.id.name, R.id.tel} );
         lv.setAdapter(sa);
     }
